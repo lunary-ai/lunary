4b7e9244846feef81b099cfa5e02c622f21e387c
Vince Lwt <vince@lyser.io>
2023-07-26 15:27:54 +0200

    simplify install

--- selfhosting/setup-dump.sql
+++ selfhosting/setup-dump.sql
@@ -1,84 +1,214 @@
--- Table Definition
-CREATE TABLE IF NOT EXISTS "public"."app" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "created_at" timestamptz DEFAULT now(),
-    "owner" uuid,
-    "name" text NOT NULL
-);
-
--- Table Definition
-CREATE TABLE IF NOT EXISTS "public"."event" (
-    "id" int8 GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    "timestamp" timestamptz DEFAULT now(),
-    "app" uuid NOT NULL,
-    "message" text,
-    "history" _jsonb,
-    "extra" jsonb,
-    "type" text,
-    "convo" uuid,
-    "model" text
-);
 
 -- Table Definition
 CREATE TABLE IF NOT EXISTS "public"."profile" (
-    "id" uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
-    "updated_at" timestamptz
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "updated_at" timestamptz,
+    PRIMARY KEY ("id")
 );
 
-
-CREATE TABLE IF NOT EXISTS "public"."convo" (
-    "id" uuid NOT NULL PRIMARY KEY,
+-- Table Definition
+CREATE TABLE "public"."app" (
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
     "created_at" timestamptz DEFAULT now(),
-    "app" uuid NOT NULL,
-    "tags" _text
+    "owner" uuid,
+    "name" text NOT NULL,
+    PRIMARY KEY ("id")
 );
 
+
+-- Table Definition
+CREATE TABLE "public"."log" (
+    "id" int8 NOT NULL,
+    "created_at" timestamptz DEFAULT now(),
+    "message" text,
+    "level" text,
+    "extra" jsonb,
+    "app" uuid NOT NULL,
+    "run" uuid,
+    PRIMARY KEY ("id")
+);
+
+-- Table Definition
+CREATE TABLE "public"."run" (
+    "created_at" timestamptz DEFAULT now(),
+    "tags" _text,
+    "app" uuid,
+    "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
+    "status" text,
+    "name" text,
+    "ended_at" timestamptz,
+    "error" jsonb,
+    "input" jsonb,
+    "output" jsonb,
+    "params" jsonb,
+    "type" text NOT NULL,
+    "parent_run" uuid,
+    "completion_tokens" int4,
+    "prompt_tokens" int4,
+    PRIMARY KEY ("id")
+);
+
+;
+;
+;
 ALTER TABLE "public"."app" ADD FOREIGN KEY ("owner") REFERENCES "auth"."users"("id") ON DELETE SET NULL;
-
-ALTER TABLE "public"."event" ADD FOREIGN KEY ("app") REFERENCES "public"."app"("id") ON DELETE CASCADE;
-ALTER TABLE "public"."event" ADD FOREIGN KEY ("convo") REFERENCES "public"."convo"("id") ON DELETE CASCADE;
-
-ALTER TABLE "public"."convo" ADD FOREIGN KEY ("app") REFERENCES "public"."app"("id") ON DELETE CASCADE;
-
-
+ALTER TABLE "public"."log" ADD FOREIGN KEY ("run") REFERENCES "public"."run"("id") ON DELETE CASCADE;
+ALTER TABLE "public"."log" ADD FOREIGN KEY ("app") REFERENCES "public"."app"("id");
 ALTER TABLE "public"."profile" ADD FOREIGN KEY ("id") REFERENCES "auth"."users"("id");
+ALTER TABLE "public"."run" ADD FOREIGN KEY ("parent_run") REFERENCES "public"."run"("id") ON DELETE SET NULL;
+ALTER TABLE "public"."run" ADD FOREIGN KEY ("app") REFERENCES "public"."app"("id") ON DELETE CASCADE;
 
 
-CREATE OR REPLACE FUNCTION public.upsert_convo(_id uuid, _app uuid, _tags text[]) RETURNS void
-LANGUAGE plpgsql
-AS $function$
-BEGIN
-    INSERT INTO convo (id, app, tags)
-    VALUES (_id, _app, _tags)
-    ON CONFLICT (id) DO NOTHING;
-END;
-$function$
+--
+-- Name: app; Type: ROW SECURITY; Schema: public; Owner: -
+--
+
+ALTER TABLE public.app ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: log; Type: ROW SECURITY; Schema: public; Owner: -
+--
+
+ALTER TABLE public.log ENABLE ROW LEVEL SECURITY;
 
 
-CREATE OR REPLACE FUNCTION public.get_convos(_app uuid, _offset integer DEFAULT 0)
- RETURNS TABLE(id uuid, app uuid, tags text[], messages bigint, calls bigint, start timestamp with time zone, "end" timestamp with time zone, first_message text)
- LANGUAGE plpgsql
-AS $function$
+--
+-- Name: profile; Type: ROW SECURITY; Schema: public; Owner: -
+--
+
+ALTER TABLE public.profile ENABLE ROW LEVEL SECURITY;
+
+--
+-- Name: run; Type: ROW SECURITY; Schema: public; Owner: -
+--
+
+ALTER TABLE public.run ENABLE ROW LEVEL SECURITY;
+
+
+--
+-- Name: profile Profiles are viewable by users who created them.; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY "Profiles are viewable by users who created them." ON public.profile FOR SELECT USING ((auth.uid() = id));
+
+
+--
+-- Name: profile Users can insert their own profile.; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY "Users can insert their own profile." ON public.profile FOR INSERT WITH CHECK ((auth.uid() = id));
+
+--
+-- Name: profile Users can update own profile.; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY "Users can update own profile." ON public.profile FOR UPDATE USING ((auth.uid() = id));
+
+
+--
+-- Name: app app_owner_policy; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY app_owner_policy ON public.app USING (auth.uid() = app.owner);
+
+
+
+--
+-- Name: log log_owner_policy; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY log_owner_policy ON public.log USING ((( SELECT app.owner
+   FROM public.app
+  WHERE (app.id = log.app)) = auth.uid()));
+
+
+--
+-- Name: run run_owner_policy; Type: POLICY; Schema: public; Owner: -
+--
+
+CREATE POLICY run_owner_policy ON public.run USING ((( SELECT app.owner
+   FROM public.app
+  WHERE (app.id = run.app)) = auth.uid()));
+
+
+
+
+--
+-- Name: get_related_runs(uuid); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.get_related_runs(run_id uuid) RETURNS TABLE(created_at timestamp with time zone, tags text[], app uuid, id uuid, status text, name text, ended_at timestamp with time zone, error jsonb, input jsonb, output jsonb, params jsonb, type text, parent_run uuid, completion_tokens integer, prompt_tokens integer)
+    LANGUAGE plpgsql
+    AS $$
 BEGIN
     RETURN QUERY
-    SELECT
-        convo.id as id,
-        convo.app as app,
-        convo.tags as tags,
-        count(*) filter (where type like '%:message%') as messages,
-        count(*) filter (where type = 'llm:call') as calls, 
-        min(timestamp) as "start", 
-        max(timestamp) as "end",
-        (select message from event where convo = convo.id order by timestamp limit 1) as first_message
+    WITH RECURSIVE related_runs AS (
+        SELECT r1.*
+        FROM run r1
+        WHERE r1.id = run_id
+
+        UNION ALL
+
+        SELECT r2.*
+        FROM run r2
+        INNER JOIN related_runs rr ON rr.id = r2.parent_run
+    )
+    SELECT rr.created_at, rr.tags, rr.app, rr.id, rr.status, rr.name, rr.ended_at, rr.error, rr.input, rr.output, 
+           rr.params, rr.type, rr.parent_run, rr.completion_tokens, rr.prompt_tokens
+    FROM related_runs rr;
+END; $$;
+
+
+--
+-- Name: get_runs_usage(uuid, integer); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.get_runs_usage(app_id uuid, days integer) RETURNS TABLE(name text, type text, completion_tokens bigint, prompt_tokens bigint, errors bigint, success bigint)
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+    RETURN QUERY 
+    SELECT 
+        run.name,
+        run.type,
+        COALESCE(SUM(run.completion_tokens), 0) AS completion_tokens,
+        COALESCE(SUM(run.prompt_tokens), 0) AS prompt_tokens,
+        SUM(CASE WHEN run.status = 'error' THEN 1 ELSE 0 END) AS errors,
+        SUM(CASE WHEN run.status = 'success' THEN 1 ELSE 0 END) AS success
     FROM 
-        convo
-        LEFT JOIN event e1 on e1.convo = convo.id
+        run
     WHERE 
-        convo.app = _app
+        run.app = app_id AND
+        run.created_at >= NOW() - INTERVAL '1 day' * days
     GROUP BY
-        convo.id, convo.app
-    ORDER BY start DESC
-    LIMIT 100 OFFSET _offset;
-END;
-$function$
+        run.name, run.type;
+END; $$;
+
+
+--
+-- Name: handle_new_user(); Type: FUNCTION; Schema: public; Owner: -
+--
+
+CREATE FUNCTION public.handle_new_user() RETURNS trigger
+    LANGUAGE plpgsql SECURITY DEFINER
+    SET search_path TO 'public'
+    AS $$
+begin
+  insert into public.profile (id)
+  values (new.id);
+  return new;
+end;
+$$;
+
+--
+-- Name: users on_auth_user_created; Type: TRIGGER; Schema: auth; Owner: -
+--
+
+CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION public.handle_new_user();
+
+--
+-- Name: idx_run_parent_run; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX idx_run_parent_run ON public.run USING btree (parent_run);
 
