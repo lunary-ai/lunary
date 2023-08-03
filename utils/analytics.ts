8fc424e62a21f84260528d9caa9463f19178b2aa
Vince Lwt <vince@lyser.io>
2023-08-03 14:57:17 +0200

    segment proxy

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -2,7 +2,18 @@ import { AnalyticsBrowser } from "@segment/analytics-next"
 
 // we can export this instance to share with rest of our codebase.
 export const analytics = process.env.NEXT_PUBLIC_SEGMENT_KEY
-  ? AnalyticsBrowser.load({
-      writeKey: process.env.NEXT_PUBLIC_SEGMENT_KEY,
-    })
+  ? AnalyticsBrowser.load(
+      {
+        writeKey: process.env.NEXT_PUBLIC_SEGMENT_KEY,
+        cdnURL: process.env.NEXT_PUBLIC_SEGMENT_CDN_URL,
+      },
+      {
+        integrations: {
+          "Segment.io": {
+            apiHost: process.env.NEXT_PUBLIC_SEGMENT_API_URL,
+            protocol: "https",
+          },
+        },
+      }
+    )
   : undefined
