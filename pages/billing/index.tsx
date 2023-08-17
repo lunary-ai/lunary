370a5779166ddc3bacb2528d3c7e1e9917d4f3ca
Vince Lwt <vince@lyser.io>
2023-08-17 14:35:18 +0200

    Update billing

--- pages/billing/index.tsx
+++ pages/billing/index.tsx
@@ -8,13 +8,39 @@ import {
   Title,
   Loader,
   Container,
+  Button,
 } from "@mantine/core"
+import { modals } from "@mantine/modals"
+import { useSupabaseClient } from "@supabase/auth-helpers-react"
 import Script from "next/script"
+import { use, useEffect, useState } from "react"
 
 export default function Billing() {
   const { profile, loading } = useProfile()
+  const supabaseClient = useSupabaseClient()
+
+  const [usage, setUsage] = useState(0)
+
+  useEffect(() => {
+    if (profile) {
+      // last 30 days of runs
+      supabaseClient
+        .from("run")
+        .select("*", { count: "estimated", head: true })
+        .gte(
+          "created_at",
+          new Date(Date.now() - 30 * 24 * 60 * 60 * 1000).toISOString()
+        )
+        .then(({ count }) => {
+          console.log(count)
+          setUsage(count)
+        })
+    }
+  }, [profile])
 
   if (loading) return <Loader />
+
+  const percent = (usage / 30000) * 100
 
   return (
     <Container>
@@ -25,15 +51,31 @@ export default function Billing() {
           You are currently on a <Badge>{profile?.plan}</Badge> plan.
         </Text>
 
+        {profile?.plan === "free" && (
+          <Button
+            onClick={() =>
+              modals.openContextModal({ modal: "upgrade", innerProps: {} })
+            }
+            w={300}
+          >
+            Upgrade to Pro
+          </Button>
+        )}
+
         <Card withBorder radius="md" padding="xl">
           <Stack spacing="sm">
             <Text fz="md" fw={700} c="dimmed">
               Monthly Queries Allowance
             </Text>
             <Text fz="lg" fw={500}>
-              ∞ / ∞ queries
+              {usage} / 30.000 requests
             </Text>
-            <Progress value={1} size="lg" radius="xl" />
+            <Progress
+              value={percent}
+              size="lg"
+              radius="xl"
+              color={percent > 99 ? "red" : "blue"}
+            />
           </Stack>
         </Card>
 
@@ -45,7 +87,7 @@ export default function Billing() {
             <Text fz="lg" fw={500}>
               1 / 1 users
             </Text>
-            <Progress value={100} size="lg" radius="xl" />
+            <Progress value={100} size="lg" color="orange" radius="xl" />
           </Stack>
         </Card>
       </Stack>
