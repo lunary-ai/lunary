30215171b9a344ddf90d1d582eaf86e0025dacce
vincelwt <vince@lyser.io>
2023-09-08 11:34:36 +0200

    🌁 Esthetic changes

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -2,7 +2,19 @@ import CopyText from "@/components/Blocks/CopyText"
 import { AppContext } from "@/utils/context"
 import { useApps } from "@/utils/supabaseHooks"
 
-import { Button, Card, Popover, Stack, Text, Title } from "@mantine/core"
+import {
+  Alert,
+  Button,
+  Card,
+  Center,
+  Group,
+  Overlay,
+  Popover,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core"
+import { IconBrandOpenai, IconCsv, IconDownload } from "@tabler/icons-react"
 import { NextSeo } from "next-seo"
 import Router from "next/router"
 import { useContext } from "react"
@@ -22,6 +34,53 @@ export default function AppAnalytics() {
             <Text>
               App ID for tracking: <CopyText value={app?.id} />
             </Text>
+          </Stack>
+        </Card>
+        <Card withBorder p="lg">
+          <Overlay blur={2} opacity={0.3} p="lg">
+            <Center h="100%">
+              <Alert title="Datasets">
+                This feature is currently invite-only. Contact us with details
+                on what you're building to request access.
+              </Alert>
+            </Center>
+          </Overlay>
+          <Stack spacing="md">
+            <Title order={4}>Export dataset</Title>
+
+            <Text weight="semibold">Select conditions:</Text>
+
+            <Group spacing="xs">
+              <Button variant="outline" compact>
+                model = "gpt-4"
+              </Button>
+
+              <Text>and</Text>
+              <Button variant="outline" compact>
+                feedback: "thumbs" = "up"
+              </Button>
+
+              <Text>and</Text>
+              <Button variant="outline" compact>
+                contains tag: "training"
+              </Button>
+            </Group>
+
+            <Text weight="semibold">Download:</Text>
+
+            <Group>
+              <Button variant="light" leftIcon={<IconDownload size={16} />}>
+                .csv
+              </Button>
+              <Button
+                variant="light"
+                color="cyan"
+                leftIcon={<IconDownload size={16} />}
+                rightIcon={<IconBrandOpenai size={16} />}
+              >
+                OpenAI .jsonl
+              </Button>
+            </Group>
           </Stack>
         </Card>
         <Card withBorder p="lg" sx={{ overflow: "visible" }}>
