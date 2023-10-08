b24c1b5c689cc4aa1598bd8dcb08739faf6034cd
hughcrt <chocart.hugues@icloud.com>
2023-10-08 09:37:40 +0200

    Test magic login

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -16,7 +16,9 @@ import UpgradeModal from "./UpgradeModal"
 
 export default function Layout({ children }: { children: ReactNode }) {
   const router = useRouter()
-  const isAuthPage = ["/login", "/signup", "/join"].includes(router.pathname)
+  const isAuthPage = ["/login", "/signup", "/join", "/magic-login"].includes(
+    router.pathname
+  )
 
   const { session, isLoading } = useSessionContext()
 
