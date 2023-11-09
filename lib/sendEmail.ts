4ce87da2a5fa9289122cf123d22589e35315269b
vincelwt <vince@lyser.io>
2023-11-10 00:13:38 +0800

    don't throw when email fails

--- lib/sendEmail.ts
+++ lib/sendEmail.ts
@@ -3,14 +3,18 @@ export const sendEmail = async (body) => {
     return console.warn("RESEND_KEY is not set, skipping email sending")
   }
 
-  const res = await fetch("https://api.resend.com/emails", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.RESEND_KEY}`,
-    },
-    body: JSON.stringify(body),
-  })
+  try {
+    const res = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.RESEND_KEY}`,
+      },
+      body: JSON.stringify(body),
+    })
 
-  return await res.json()
+    return await res.json()
+  } catch (e) {
+    console.error("Error sending email", e)
+  }
 }
