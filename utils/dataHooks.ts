c3d579b44f71803fcadc8c5ce8b93c1cbe44c815
hughcrt <chocart.hugues@icloud.com>
2023-11-21 15:43:13 +0000

    Update getTraces rpc

--- utils/dataHooks.ts
+++ utils/dataHooks.ts
@@ -253,6 +253,7 @@ export function useTraces(search) {
       .is("parent_run", null)
   } else {
     query = supabaseClient.rpc("get_trace_runs_roots", {
+      appId,
       search_pattern: search,
     })
   }
