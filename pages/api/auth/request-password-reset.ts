2b3977b463f0281502e82bdbc4d8fe05af425915
hughcrt <chocart.hugues@icloud.com>
2023-12-14 13:28:11 +0100

    🌎 Add custom token to password reset instead of relying on supabase one

--- pages/api/auth/request-password-reset.ts
+++ pages/api/auth/request-password-reset.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import sql from "../../../lib/db"
 import { RESET_PASSWORD } from "../../../lib/emails"
 import { sendEmail } from "../../../lib/sendEmail"
+import { randomUUID } from "crypto"
 
 export default async function handler(
   req: NextApiRequest,
@@ -9,12 +10,13 @@ export default async function handler(
 ) {
   const { email } = await JSON.parse(req.body)
 
-  const [{ recovery_token: token }] =
-    await sql`select recovery_token from auth.users where email = ${email}`
+  const recoveryToken = randomUUID()
+  await sql`update auth.users set recovery_token = ${recoveryToken} where email = ${email}`
+
 
   const confirmLink = `${
     process.env.NEXT_PUBLIC_APP_URL || "http://localhost:8080"
-  }/reset-password?token=${token}&email=${email}`
+  }/reset-password?token=${recoveryToken}&email=${email}`
 
   await sendEmail(RESET_PASSWORD(email, confirmLink))
   console.log("ok")
