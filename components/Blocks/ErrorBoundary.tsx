d66afec8d30e47e8e689bd96fccf6eeb622f54fa
hughcrt <chocart.hugues@icloud.com>
2023-12-21 20:27:49 +0100

    ⚡️ Add option to hide highlight error dialog

--- components/Blocks/ErrorBoundary.tsx
+++ components/Blocks/ErrorBoundary.tsx
@@ -18,19 +18,15 @@ class ErrorBoundary extends React.Component<
   }
 
   static getDerivedStateFromError(error: Error): ErrorBoundaryState {
-    // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    // You can also log the error to an error reporting service
     console.error(error, errorInfo)
-    // logErrorToMyService(error, errorInfo)
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
       return <p>Error rendering this component</p>
     }
 
