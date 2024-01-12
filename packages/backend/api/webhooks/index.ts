237228b30f7efe08f27ec9ac6c18791f9df21d89
hughcrt <chocart.hugues@icloud.com>
2024-01-12 12:37:54 -0300

    fix(backend): remove signup webhook

--- packages/backend/api/webhooks/index.ts
+++ packages/backend/api/webhooks/index.ts
@@ -1,12 +1,10 @@
 import Router from "koa-router"
 import stripe from "./stripe"
-import signup from "./signup"
 
 const webhooks = new Router({
   prefix: "/webhooks",
 })
 
 webhooks.use(stripe.routes())
-webhooks.use(signup.routes())
 
 export default webhooks
