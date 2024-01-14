d21362a892e2c24e0416b42ee974881946a3027e
vincelwt <vince@lyser.io>
2024-01-14 19:40:19 +0100

    feat: evals prompt editor

--- packages/frontend/components/Prompts/TemplateInputArea.tsx
+++ packages/frontend/components/Prompts/TemplateInputArea.tsx
@@ -1,29 +1,9 @@
-import {
-  Box,
-  Loader,
-  Stack,
-  Textarea,
-  Text,
-  ActionIcon,
-  ScrollArea,
-  Group,
-} from "@mantine/core"
-import { IconCircleMinus, IconCirclePlus } from "@tabler/icons-react"
-import { ChatMessage } from "@/components/SmartViewer/Message"
+import { Text, ScrollArea, Group } from "@mantine/core"
 import SmartViewer from "@/components/SmartViewer"
 import TokensBadge from "../Blocks/TokensBadge"
+import { PromptEditor } from "./PromptEditor"
 
-interface TemplateInputAreaProps {
-  template: any
-  setTemplate: (template: any) => void
-  setHasChanges: (hasChanges: boolean) => void
-  saveTemplate: () => void
-  outputTokens: number
-  output: any
-  error: any
-}
-
-const TemplateInputArea: React.FC<TemplateInputAreaProps> = ({
+function TemplateInputArea({
   template,
   setTemplate,
   saveTemplate,
@@ -31,98 +11,38 @@ const TemplateInputArea: React.FC<TemplateInputAreaProps> = ({
   output,
   outputTokens,
   error,
-}) => {
+}: {
+  template: any
+  setTemplate: (template: any) => void
+  saveTemplate: () => void
+  setHasChanges: (hasChanges: boolean) => void
+  output: any
+  outputTokens: number
+  error: any
+}) {
   const isText = typeof template?.content === "string"
+  const handleContentChange = (newContent: any) => {
+    setTemplate({ ...template, content: newContent })
+    setHasChanges(true)
+  }
 
   return (
     <ScrollArea h="100%">
-      {isText ? (
-        <Textarea
-          value={template?.content ?? ""}
-          onChange={(e) => {
-            setTemplate({ ...template, content: e.currentTarget.value })
-            setHasChanges(true)
-          }}
-          minRows={5}
-          onKeyDown={(e) => {
-            if ((e.metaKey || e.ctrlKey) && e.key === "s") {
-              e.preventDefault()
-              // Propagate to parent component or handle the save operation here
-              saveTemplate()
-            }
-          }}
-          autosize
-        />
-      ) : (
-        <Stack>
-          <Text fw="bold" size="sm">
-            Input
-          </Text>
-          {typeof template?.content !== "undefined" &&
-            Array.isArray(template?.content) &&
-            template?.content?.map((message, i) => (
-              <Box pos="relative" key={i}>
-                <ChatMessage
-                  data={message}
-                  key={i}
-                  editable={true}
-                  onChange={(newMessage) => {
-                    const newContent = template?.content
-                      ? [...template?.content]
-                      : []
-                    newContent[i] = newMessage
-                    setTemplate({ ...template, content: newContent })
-                    setHasChanges(true)
-                  }}
-                />
-                <ActionIcon
-                  pos="absolute"
-                  top={4}
-                  right={4}
-                  size="sm"
-                  color="red"
-                  variant="transparent"
-                  onClick={() => {
-                    const newContent = [...template?.content]
-                    newContent.splice(i, 1)
-                    setTemplate({ ...template, content: newContent })
-                    setHasChanges(true)
-                  }}
-                >
-                  <IconCircleMinus size="12" />
-                </ActionIcon>
-              </Box>
-            ))}
-
-          <ActionIcon
-            mx="auto"
-            mt="xs"
-            variant="transparent"
-            color="gray"
-            onClick={() => {
-              const newContent = [
-                ...(template?.content || []),
-                { content: " ", role: "user" },
-              ]
-              setTemplate({ ...template, content: newContent })
-            }}
-          >
-            <IconCirclePlus size="16" />
-          </ActionIcon>
-
-          {(output || error) && (
-            <>
-              <Group justify="space-between">
-                <Text fw="bold" size="sm">
-                  {error ? "Error" : "Output"}
-                </Text>
-                {outputTokens && <TokensBadge tokens={outputTokens} />}
-              </Group>
-
-              <SmartViewer data={output} error={error} />
-            </>
-          )}
-        </Stack>
+      <PromptEditor
+        value={template?.content}
+        onChange={handleContentChange}
+        isText={isText}
+      />
+      {(output || error) && (
+        <>
+          <Group justify="space-between">
+            <Text fw="bold" size="sm">
+              {error ? "Error" : "Output"}
+            </Text>
+            {outputTokens && <TokensBadge tokens={outputTokens} />}
+          </Group>
+          <SmartViewer data={output} error={error} />
+        </>
       )}
     </ScrollArea>
   )
--- packages/frontend/pages/evals/index.tsx
+++ packages/frontend/pages/evals/index.tsx
@@ -1,19 +1,25 @@
 import Steps from "@/components/Blocks/Steps"
 import FilterPicker, { SavedFilterData } from "@/components/Filters/Picker"
 import Paywall from "@/components/Layout/Paywall"
+import { PromptEditor } from "@/components/Prompts/PromptEditor"
 import { useTemplates } from "@/utils/dataHooks"
+import { usePromptVariables } from "@/utils/promptsHooks"
 import {
   Badge,
   Button,
   Chip,
   Container,
+  Fieldset,
   Group,
+  Modal,
   MultiSelect,
   Stack,
   Text,
+  TextInput,
+  Textarea,
   Title,
 } from "@mantine/core"
-import { useSetState } from "@mantine/hooks"
+import { useListState, useSetState } from "@mantine/hooks"
 import {
   IconDatabase,
   IconFlask2Filled,
@@ -21,6 +27,7 @@ import {
 } from "@tabler/icons-react"
 import Link from "next/link"
 import Router from "next/router"
+import { useEffect, useState } from "react"
 import { MODELS } from "shared"
 
 const FEATURE_LIST = [
@@ -29,8 +36,147 @@ const FEATURE_LIST = [
   "Compare results with OpenAI, Anthropic, Mistral and more",
 ]
 
+type Variation = {
+  variables: any
+  context?: string
+  expected?: string
+}
+
+function AddPromptModal({ opened, setOpened, onAdd }) {
+  // const [value, setValue] = useState("")
+  const [prompt, setPrompt] = useState([
+    {
+      role: "system",
+      content: "You are a helpful assistant.",
+    },
+    {
+      role: "user",
+      content: "{{question}}",
+    },
+  ])
+
+  const [variations, handlers] = useListState<Variation>([
+    {
+      variables: {},
+    },
+  ])
+
+  const variables = usePromptVariables(prompt)
+
+  const hasVariables = Object.keys(variables).length > 0
+
+  useEffect(() => {
+    if (hasVariables && !variations[0].variables) {
+      handlers.setItemProp(
+        0,
+        "variables",
+        Object.keys(variables).reduce(
+          (acc, key) => ({ ...acc, [key]: "" }),
+          {},
+        ),
+      )
+    }
+  }, [variables])
+
+  return (
+    <Modal
+      opened={opened}
+      onClose={() => setOpened(false)}
+      size="lg"
+      title="Add a new prompt"
+    >
+      <Stack>
+        <PromptEditor
+          onChange={(value) => setPrompt(value)}
+          value={prompt}
+          isText={false}
+        />
+
+        {/* Show this if variables in prompt, add the possibility to add variations of the variables */}
+        {hasVariables && (
+          <>
+            <Text size="sm" color="gray">
+              Test different variations of the variables in the prompt:
+            </Text>
+          </>
+        )}
+        {variations.map((variation, index) => (
+          <Fieldset
+            variant="filled"
+            key={index}
+            legend={hasVariables && `Variation ${index + 1}`}
+          >
+            <Stack>
+              {Object.keys(variables).map((variable) => (
+                <TextInput
+                  key={variable}
+                  label={variable}
+                  value={variation[variable]}
+                  onChange={(e) =>
+                    handlers.setItemProp(index, variable, e.currentTarget.value)
+                  }
+                />
+              ))}
+              <Textarea
+                label="Context (optional)"
+                description="Can be used to evaluate if the LLM response is factually correct and on topic."
+                required={false}
+                value={variation.context}
+                onChange={(e) =>
+                  handlers.setItemProp(index, "context", e.currentTarget.value)
+                }
+              />
+
+              <Textarea
+                label="Gold / expected output (optional)"
+                description="Can be used to evaluate if the LLM response is close to the expected output."
+                required={false}
+                value={variation.expected}
+                onChange={(e) =>
+                  handlers.setItemProp(index, "expected", e.currentTarget.value)
+                }
+              />
+            </Stack>
+          </Fieldset>
+        ))}
+
+        {hasVariables && (
+          <Button
+            variant="outline"
+            onClick={() =>
+              handlers.append({
+                variables: Object.keys(variables).reduce(
+                  (acc, key) => ({ ...acc, [key]: "" }),
+                  {},
+                ),
+              })
+            }
+          >
+            Add variation of variables
+          </Button>
+        )}
+
+        <Button
+          display="inline-block"
+          ml="auto"
+          onClick={() =>
+            onAdd({
+              prompt,
+              variations,
+            })
+          }
+        >
+          Add prompt
+        </Button>
+      </Stack>
+    </Modal>
+  )
+}
+
 export default function Evals() {
   const { templates } = useTemplates()
+
+  const [promptModalOpened, setPromptModalOpened] = useState(false)
 
   const [evaluation, setEvaluation] = useSetState<{
     prompts: any[]
@@ -94,11 +240,26 @@ export default function Evals() {
           </Text>
 
           <Steps>
-            <Steps.Step n={1} label="Prompt">
+            <Steps.Step n={1} label="Dataset">
               <Text size="lg" mb="md">
-                We recommend using a template as a starting point.
+                Add prompts and variations of variables to test.
               </Text>
-              <Group>
+              <AddPromptModal
+                opened={promptModalOpened}
+                setOpened={setPromptModalOpened}
+                onAdd={(prompt) => {
+                  setEvaluation({
+                    prompts: [...evaluation.prompts, prompt],
+                  })
+                }}
+              />
+              <Button
+                variant="light"
+                onClick={() => setPromptModalOpened(true)}
+              >
+                Add prompt
+              </Button>
+              {/* <Group>
                 <Chip.Group>
                   {templates?.map((template) => (
                     <Chip
@@ -119,8 +280,8 @@ export default function Evals() {
                 </Button>
                 <Button variant="purple" disabled href="/logs" component={Link}>
                   Import from logs
-                </Button>
-              </Group>
+                </Button> 
+              </Group> */}
             </Steps.Step>
             <Steps.Step n={2} label="Models">
               <Text size="lg" mb="md">
