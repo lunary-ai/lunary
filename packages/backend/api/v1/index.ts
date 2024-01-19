8209b2d1e5b3095847ce4ffb499b178ee14559ec
vincelwt <vince@lyser.io>
2024-01-19 23:19:39 +0100

    add health endpoint

--- packages/backend/api/v1/index.ts
+++ packages/backend/api/v1/index.ts
@@ -9,6 +9,7 @@ import templateVersions from "./templateVersions"
 import templates from "./templates"
 import users from "./users"
 import projectUsers from "./external-users"
+import sql from "@/utils/db"
 
 const v1 = new Router({
   prefix: "/v1",
@@ -16,6 +17,16 @@ const v1 = new Router({
 
 v1.get("/", async (ctx) => {
   ctx.body = "Lunary API v1"
+})
+
+v1.get("/health", async (ctx) => {
+  const [last] =
+    await sql`select created_at from run order by created_at desc limit 1`
+
+  const olderThanTenMinutes =
+    new Date(last.created_at).getTime() < Date.now() - 10 * 60 * 1000
+
+  ctx.body = olderThanTenMinutes ? "STALE" : "OK"
 })
 
 v1.use(orgs.routes())
