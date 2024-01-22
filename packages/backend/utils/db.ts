4de00f4d2ab4f88cadcb388a3f45f87b3cd24459
hughcrt <chocart.hugues@icloud.com>
2024-01-22 12:59:02 -0300

    chore: add timeout and lifetime limit to the sql client

--- packages/backend/utils/db.ts
+++ packages/backend/utils/db.ts
@@ -1,6 +1,8 @@
 import postgres from "postgres"
 
 const sql = postgres(process.env.DATABASE_URL!, {
+  idle_timeout: 20,
+  max_lifetime: 60 * 5,
   transform: {
     ...postgres.camel,
     undefined: null,
