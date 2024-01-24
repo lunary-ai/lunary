363312ad1cdac58f6f18e575f639e89a01b57031
hughcrt <chocart.hugues@icloud.com>
2024-01-24 17:51:48 -0300

    chore: add db migration script

--- packages/db/migration.sql
+++ packages/db/migration.sql
@@ -1,7 +1,15 @@
 update
-	"public"."api_key"
+	api_key
 set
-	"api_key" = "project_id"
+	api_key = project_id
 where
-	"type" = 'public'; 
+	type = 'public'; 
+
+alter table api_key 
+drop constraint "api_key_project_id_fkey", 
+add constraint "api_key_project_id_fkey" foreign key (project_id) references project (id) on delete cascade;
+
+alter table external_user 
+drop constraint "external_user_project_id_fkey", 
+add constraint "external_user_project_id_fkey" foreign key (project_id) references project (id) on delete cascade;
 
