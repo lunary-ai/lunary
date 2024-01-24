64de0b7f280a2c8eb355b869c8d556c88341f30e
vincelwt <vince@lyser.io>
2024-01-24 10:35:25 +0100

    fix: switch stripe portal to backend

--- packages/frontend/pages/api/app/[id].ts
+++ packages/frontend/pages/api/app/[id].ts
@@ -1,10 +1,8 @@
-import { apiWrapper } from "@/lib/api/helpers"
-
 import { NextApiRequest, NextApiResponse } from "next"
 
-export default apiWrapper(async function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   return res.status(200).json("ok")
-})
+}
