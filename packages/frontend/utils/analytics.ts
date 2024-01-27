3faf9f605596df5f33cb27f7ae757b845957e115
vincelwt <vince@lyser.io>
2024-01-27 15:14:33 +0100

    fix: limited UI bar

--- packages/frontend/utils/analytics.ts
+++ packages/frontend/utils/analytics.ts
@@ -66,13 +66,13 @@ const identify = (userId: string, traits: any) => {
   try {
     posthog?.identify(userId, traits)
 
-    // TODO: w.crisp.push undefined?
     if (!w?.crisp?.push) return
 
-    CRISP_TOKEN_ID = userId
-    CRISP_RUNTIME_CONFIG = {
-      session_merge: true,
-    }
+    if (typeof CRISP_TOKEN_ID !== "undefined") CRISP_TOKEN_ID = userId
+    if (typeof CRISP_RUNTIME_CONFIG !== "undefined")
+      CRISP_RUNTIME_CONFIG = {
+        session_merge: true,
+      }
 
     if (traits.email) w?.crisp?.push(["set", "user:email", traits.email])
     if (traits.name) w?.crisp?.push(["set", "user:nickname", traits.name])
