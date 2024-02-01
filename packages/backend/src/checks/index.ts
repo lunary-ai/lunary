4688471a2bee04bb951bafb17a5e24c6750a4a8c
vincelwt <vince@lyser.io>
2024-02-01 23:17:05 +0100

    chore: hide json

--- packages/backend/src/checks/index.ts
+++ packages/backend/src/checks/index.ts
@@ -109,6 +109,7 @@ export const CHECK_RUNNERS: CheckRunner[] = [
   },
   {
     id: "json",
+    soon: true,
     evaluator: async (run, params) => {
       const { type } = params
       let passed = false
