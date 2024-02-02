8d226114f55365ff9cffa4e9845fc269216078f4
hughcrt <chocart.hugues@icloud.com>
2024-02-02 11:15:01 -0300

    chore: batch size

--- packages/backend/src/jobs/radar.ts
+++ packages/backend/src/jobs/radar.ts
@@ -9,8 +9,8 @@ type RadarResults = {
   details?: any
 }
 
-const RUNS_BATCH_SIZE = 1000
-const PARALLEL_BATCH_SIZE = 10
+const RUNS_BATCH_SIZE = 20
+const PARALLEL_BATCH_SIZE = 3
 
 const hasNonSQLFilter = (checks: FilterLogic): boolean =>
   checks.some((check) => {
