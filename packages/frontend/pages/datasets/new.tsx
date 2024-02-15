a0e29b899315e2c6d555dc50662d8f5fb811988f
Vince Loewe <vince@lunary.ai>
2024-02-14 20:45:39 -0800

    fix create dataset

--- packages/frontend/pages/datasets/new.tsx
+++ packages/frontend/pages/datasets/new.tsx
@@ -2,13 +2,16 @@
 
 import { useDatasets } from "@/utils/dataHooks"
 import Router from "next/router"
+import { generateSlug } from "random-word-slugs"
 import { useEffect } from "react"
 
 export default function NewDataset() {
   const { insert } = useDatasets()
   useEffect(() => {
     const insertDataset = async () => {
-      const { id } = await insert()
+      const { id } = await insert({
+        slug: generateSlug(),
+      })
       Router.push(`/datasets/${id}`)
     }
     insertDataset()
