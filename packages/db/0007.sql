74d6ed76f352677d5987d7a382189756ef9e7870
Vince Loewe <vince@lunary.ai>
2024-03-28 15:53:41 +0900

    add migrations for fkeys

--- packages/db/0007.sql
+++ packages/db/0007.sql
@@ -31,3 +31,36 @@ group by
 
 create unique index on feedback_cache (project_id, type, feedback);
 create index on feedback_cache(project_id);
+
+
+-- Fix setting owner id nulls
+
+ALTER TABLE evaluation
+    ALTER COLUMN owner_id DROP NOT NULL,
+    ALTER COLUMN dataset_id DROP NOT NULL;
+
+ALTER TABLE evaluation
+    DROP CONSTRAINT evaluation_owner_id_fkey,
+    ADD CONSTRAINT evaluation_owner_id_fkey
+        FOREIGN KEY (owner_id) REFERENCES account(id) ON DELETE SET NULL;
+
+ALTER TABLE evaluation
+    DROP CONSTRAINT evaluation_dataset_id_fkey,
+    ADD CONSTRAINT evaluation_dataset_id_fkey
+        FOREIGN KEY (dataset_id) REFERENCES dataset(id) ON DELETE SET NULL;
+
+ALTER TABLE checklist
+    ALTER COLUMN owner_id DROP NOT NULL;
+
+ALTER TABLE checklist
+    DROP CONSTRAINT fk_checklist_owner_id,
+    ADD CONSTRAINT checklist_owner_id_fkey
+        FOREIGN KEY (owner_id) REFERENCES account(id) ON DELETE SET NULL;
+
+ALTER TABLE dataset
+    ALTER COLUMN owner_id DROP NOT NULL;
+
+ALTER TABLE dataset
+    DROP CONSTRAINT dataset_owner_id_fkey,
+    ADD CONSTRAINT dataset_owner_id_fkey
+        FOREIGN KEY (owner_id) REFERENCES account(id) ON DELETE SET NULL;
