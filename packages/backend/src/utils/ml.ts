f55a29ee80237dc42999f210030af9cab85a5c16
Hugues Chocart <chocart.hugues@icloud.com>
2024-03-31 23:02:04 -0300

    chore: docker version bump (#170)

--- packages/backend/src/utils/ml.ts
+++ packages/backend/src/utils/ml.ts
@@ -1,9 +1,6 @@
 export async function callML(method: string, data: any) {
-  const response = await fetch(`http://${process.env.ML_URL}/${method}`, {
+  const response = await fetch(`${process.env.ML_URL}/${method}`, {
     method: "POST",
-    // For example at the first ML calls, it needs to DL the models, so it can take a while
-    // So add timeout
-    signal: AbortSignal.timeout(5000),
     headers: {
       "Content-Type": "application/json",
     },
