88f72897a54de9c479ede3f80cba490438dbe2e7
hughcrt <chocart.hugues@icloud.com>
2024-04-01 11:54:05 -0300

    ok

--- packages/backend/src/jobs/radar.ts
+++ packages/backend/src/jobs/radar.ts
@@ -101,6 +101,7 @@ export default async function runRadarJob() {
     try {
       await radarJob()
     } catch (error) {
+      await sleep(3000) // To avoid spamming the ml service when there are connection errors
       console.error(error)
     }
   }
