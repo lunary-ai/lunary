7643497c5dc7c1adbcb573038f40a75542426116
Hugues Chocart <chocart.hugues@icloud.com>
2024-04-13 20:50:49 -0300

    chore: prepare release (#210)

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -16,6 +16,7 @@ import { setDefaultBody } from "./utils/misc"
 import ratelimit from "./utils/ratelimit"
 import { initSentry, requestHandler, tracingMiddleWare } from "./utils/sentry"
 import licenseMiddleware from "./utils/license"
+import config from "./utils/config"
 
 checkDbConnection()
 setupCronJobs()
@@ -38,7 +39,7 @@ app.use(ratelimit)
 app.use(bodyParser())
 app.use(setDefaultBody)
 
-if (process.env.IS_SELF_HOSTED) {
+if (config.IS_SELF_HOSTED) {
   app.use(licenseMiddleware)
 }
 
---
+++ packages/backend/src/utils/config.ts
@@ -0,0 +1,5 @@
+const config = {
+  IS_SELF_HOSTED: process.env.IS_SELF_HOSTED === "true" ? true : false,
+}
+
+export default config
