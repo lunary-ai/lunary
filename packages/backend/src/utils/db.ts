468b9efd3d2a5affba666f3b61eeac733c2c44f1
hughcrt <chocart.hugues@icloud.com>
2024-05-20 12:35:23 -0300

    Ok

--- packages/backend/src/utils/db.ts
+++ packages/backend/src/utils/db.ts
@@ -6,8 +6,10 @@ const sql = postgres(process.env.DATABASE_URL!, {
   idle_timeout: 20,
   max_lifetime: 60 * 5,
   transform: {
-    ...postgres.camel,
-
+    column: {
+      from: postgres.camel.column.from,
+      to: postgres.camel.column.to,
+    },
     undefined: null,
   },
   max: isProduction ? 50 : 5,
