2c9b9bd697209155afd12a62a0ccafb6fcb16433
Vince Loewe <vince@lunary.ai>
2024-05-26 21:08:05 +0200

    fix syntax

--- packages/backend/src/evaluators/replies.ts
+++ packages/backend/src/evaluators/replies.ts
@@ -1,15 +1,15 @@
 import { Run } from "shared"
-import { isOpenAIMessage, lastMsg } from "../checks"
+import { lastMsg } from "../checks"
 import openai from "@/src/utils/openai"
 import lunary from "lunary"
 
 export default async function evaluate(run: Run) {
-  
-  if (!run.input || !run.output) throw new Error("No input or output for evaluator 'replies'")
+  if (!run.input || !run.output)
+    throw new Error("No input or output for evaluator 'replies'")
 
   const template = await lunary.renderTemplate("replied-question", {
     question: lastMsg(run.input),
-    answer: lastMsg(run.output)
+    answer: lastMsg(run.output),
   })
 
   const res = await openai.chat.completions.create(template)
@@ -20,5 +20,5 @@ export default async function evaluate(run: Run) {
 
   const result = output.split("\n")[0].toLowerCase().replace(".", "").trim()
 
-  return result.includes("yes"),
+  return result.includes("yes")
 }
