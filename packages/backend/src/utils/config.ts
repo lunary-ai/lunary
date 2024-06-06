c6285f877e00e42a6c6c44fc619d47b27e76a576
Hugues Chocart <chocart.hugues@icloud.com>
2024-06-06 14:11:47 -0300

    fix: lunary-ee (#353)

--- packages/backend/src/utils/config.ts
+++ packages/backend/src/utils/config.ts
@@ -1,6 +1,6 @@
 const config = {
-  isSelfHosted: process.env.IS_SELF_HOSTED === "true" ? true : false,
-  skipEmailVerify: process.env.SKIP_EMAIL_VERIFY === "true" ? true : false,
+  IS_SELF_HOSTED: process.env.IS_SELF_HOSTED === "true" ? true : false,
+  SKIP_EMAIL_VERIFY: process.env.SKIP_EMAIL_VERIFY === "true" ? true : false,
 }
 
 export default config
