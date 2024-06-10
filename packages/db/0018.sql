efd52f3a999272a3405f03a18f5df4940d9b764b
hughcrt <chocart.hugues@icloud.com>
2024-06-10 19:20:39 -0300

    ok

--- packages/db/0018.sql
+++ packages/db/0018.sql
@@ -1,3 +1,21 @@
+create table evaluator (
+    id uuid not null default uuid_generate_v4(),
+    created_at timestamptz default now(),
+    updated_at timestamptz default now(),
+    project_id uuid not null,
+    owner_id uuid,
+    name varchar not null,
+    slug varchar not null,
+    type varchar not null,
+    mode varchar,
+    description text,
+    params jsonb,
+    filters jsonb,
+    constraint evaluator_owner_id_fkey foreign key (owner_id) references account(id) on delete set null,
+    constraint evaluator_project_id_fkey foreign key (project_id) references project(id) on delete cascade,
+    primary key (id)
+);
+
 create table evaluation_result_v2 (
     run_id uuid not null,
     evaluator_id uuid not null,
