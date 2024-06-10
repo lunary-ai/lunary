c7e444e55b7407c6203a413742049d649e4ac22b
Vincelwt <vincelwt@users.noreply.github.com>
2024-06-10 16:29:55 +0100

    feat: button to remove user data LLM-1410 (#359)

--- packages/frontend/pages/users/[id].tsx
+++ packages/frontend/pages/users/[id].tsx
@@ -1,6 +1,15 @@
 import { useRouter } from "next/router"
 
-import { Box, Card, Group, SimpleGrid, Stack, Text, Title } from "@mantine/core"
+import {
+  Box,
+  Button,
+  Card,
+  Group,
+  SimpleGrid,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core"
 
 import SmartViewer from "@/components/SmartViewer"
 
@@ -25,6 +34,11 @@ import {
 import { formatAppUser } from "@/utils/format"
 import { NextSeo } from "next-seo"
 import DataTable from "@/components/blocks/DataTable"
+import { IconCheck, IconTrash } from "@tabler/icons-react"
+import { modals } from "@mantine/modals"
+import { fetcher } from "@/utils/fetcher"
+import errorHandler from "@/utils/errors"
+import { notifications } from "@mantine/notifications"
 
 const columns = [
   timeColumn("createdAt"),
@@ -54,49 +68,98 @@ export default function UserDetails({}) {
     loadMore,
   } = useProjectInfiniteSWR(`/runs?users=${id}`)
 
+  const confirmDelete = () => {
+    modals.openConfirmModal({
+      title: "Please confirm your action",
+      confirmProps: { color: "red", "data-testid": "confirm" },
+      children: (
+        <Text size="sm">
+          Are you sure you want to delete this user data? This action cannot be
+          undone and all the user data and logs will be lost forever.
+        </Text>
+      ),
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      onConfirm: async () => {
+        const notifId = notifications.show({
+          loading: true,
+          title: "Deleting user data",
+          message: "Your user data is being deleted",
+          autoClose: false,
+          withCloseButton: false,
+        })
+
+        await fetcher.delete(`/external-users/${id}`)
+
+        notifications.update({
+          id: notifId,
+          color: "teal",
+          title: "Data removed",
+          message: "User data and logs have been successfully removed.",
+          icon: <IconCheck size={18} />,
+          loading: false,
+          autoClose: 2000,
+        })
+
+        router.push("/users")
+      },
+    })
+  }
+
   return (
     <Stack>
       <NextSeo title={formatAppUser(user)} />
 
       <Card withBorder>
-        <Group gap={48}>
-          <Group>
-            <AppUserAvatar user={user} />
-            <Title order={4}>{formatAppUser(user)}</Title>
-          </Group>
-          <Group gap={3}>
-            <Text>ID:</Text>
-            <CopyText value={user?.externalId} />
-          </Group>
-          {email && (
-            <Group gap={3}>
-              <Text>Email:</Text>
-              <CopyText value={email} />
+        <Group justify="space-between" align="center">
+          <Group gap={48}>
+            <Group>
+              <AppUserAvatar user={user} />
+              <Title order={4}>{formatAppUser(user)}</Title>
             </Group>
-          )}
-          <Group>
-            {user?.last_seen && (
-              <Text c="dimmed">{`last seen:  ${new Date(
-                user.last_seen,
-              ).toLocaleString(undefined, {
-                month: "short",
-                day: "numeric",
-                hour: "numeric",
-                minute: "numeric",
-              })}`}</Text>
+            <Group gap={3}>
+              <Text>ID:</Text>
+              <CopyText value={user?.externalId} />
+            </Group>
+            {email && (
+              <Group gap={3}>
+                <Text>Email:</Text>
+                <CopyText value={email} />
+              </Group>
+            )}
+            <Group>
+              {user?.last_seen && (
+                <Text c="dimmed">{`last seen:  ${new Date(
+                  user.last_seen,
+                ).toLocaleString(undefined, {
+                  month: "short",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                })}`}</Text>
+              )}
+            </Group>
+
+            {Object.keys(extraProps).length > 0 && (
+              <SmartViewer data={extraProps} />
             )}
           </Group>
-
-          {Object.keys(extraProps).length > 0 && (
-            <SmartViewer data={extraProps} />
-          )}
+          <Button
+            leftSection={<IconTrash size={14} />}
+            size="xs"
+            color="red"
+            variant="light"
+            onClick={() => {
+              confirmDelete()
+            }}
+          >
+            Remove Data
+          </Button>
         </Group>
       </Card>
-      <Title order={2}>Analytics</Title>
+
       {usage && (
         <SimpleGrid cols={3} spacing="md">
           <UsageSummary usage={usage} />
-          <AgentSummary usage={usage} />
         </SimpleGrid>
       )}
 
