c9207c26312c5c404c48702da7505006249aa7f5
hughcrt <chocart.hugues@icloud.com>
2024-06-10 12:52:23 -0300

    merge conflict

--- packages/frontend/pages/users/[id].tsx
+++ packages/frontend/pages/users/[id].tsx
@@ -16,6 +16,7 @@ import SmartViewer from "@/components/SmartViewer"
 import UsageSummary from "@/components/analytics/UsageSummary"
 import AppUserAvatar from "@/components/blocks/AppUserAvatar"
 import CopyText from "@/components/blocks/CopyText"
+import DataTable from "@/components/blocks/DataTable"
 import {
   useProjectInfiniteSWR,
   useProjectSWR,
@@ -31,18 +32,12 @@ import {
   tagsColumn,
   timeColumn,
 } from "@/utils/datatable"
-import { formatAppUser } from "@/utils/format"
-import { NextSeo } from "next-seo"
-import DataTable from "@/components/blocks/DataTable"
-<<<<<<< Updated upstream
-import { IconCheck, IconTrash } from "@tabler/icons-react"
-import { modals } from "@mantine/modals"
 import { fetcher } from "@/utils/fetcher"
-import errorHandler from "@/utils/errors"
+import { formatAppUser } from "@/utils/format"
+import { modals } from "@mantine/modals"
 import { notifications } from "@mantine/notifications"
-=======
-import AgentSummary from "@/components/analytics/AgentSummary"
->>>>>>> Stashed changes
+import { IconCheck, IconTrash } from "@tabler/icons-react"
+import { NextSeo } from "next-seo"
 
 const columns = [
   timeColumn("createdAt"),
