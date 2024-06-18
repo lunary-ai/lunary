c3dc26e178ec75800c6e05b041fec7d2d125544e
hughcrt <chocart.hugues@icloud.com>
2024-06-17 21:31:53 -0300

    test

---
+++ packages/frontend/next.config-monolith.js
@@ -0,0 +1,64 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  async rewrites() {
+    const redirects =  [
+      {
+        source: "/ingest/:path*",
+        destination: "https://app.posthog.com/:path*",
+      },
+      {
+        source: "/api/v1/report",
+        destination: process.env.API_URL + "/api/report",
+      },
+      {
+        source: "/api/report",
+        destination: process.env.API_URL + "/api/report",
+      },
+      {
+        source: "/api/v1/template",
+        destination: process.env.API_URL + "/api/v1/template",
+      },
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3333/api/:path*'
+
+      }
+    ]
+
+
+    return redirects
+  },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
+    )
+
+    return config
+  },
+  transpilePackages: ["shared"],
+}
+
+module.exports = nextConfig
