8b361209a73dbeeb32ec6304f0ccf146eca7c379
hughcrt <chocart.hugues@icloud.com>
2024-07-17 16:46:31 +0100

    ok

--- packages/frontend/sentry.server.config.ts
+++
@@ -1,6 +0,0 @@
-import * as Sentry from "@sentry/nextjs"
-
-Sentry.init({
-  dsn: "https://39824297fdaaaa06ab62a4d077cdfc8b@o4506599397588992.ingest.us.sentry.io/4506599400407040",
-  tracesSampleRate: 1.0,
-})
