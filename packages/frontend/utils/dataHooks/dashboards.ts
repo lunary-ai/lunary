83e33dff0fcc6a86fff41371420952f5093171fe
7HR4IZ3 <gamerxville@gmail.com>
2024-10-13 02:38:52 -0700

    feat: custom charts v1 and draggable dashboard charts

---
+++ packages/frontend/utils/dataHooks/dashboards.ts
@@ -0,0 +1,65 @@
+import { hasAccess } from "shared";
+import { useProjectMutation, useProjectSWR, useUser } from ".";
+import { fetcher } from "../fetcher";
+
+export function useDashboards() {
+  const { user } = useUser();
+  const { data, isLoading, mutate } = useProjectSWR(
+    hasAccess(user?.role, "dashboards", "list") ? `/dashboards` : null,
+  );
+
+  const { trigger: insert, isMutating: isInserting } = useProjectMutation(
+    `/dashboards`,
+    fetcher.post,
+  );
+
+  return {
+    dashboards: data,
+    insert,
+    isInserting,
+    mutate,
+    loading: isLoading,
+  };
+}
+
+export function useDashboard(id: string | null, initialData?: any) {
+  const { mutate: mutateViews } = useDashboards();
+
+  const {
+    data: dashboard,
+    isLoading,
+    mutate,
+  } = useProjectSWR(id && `/dashboards/${id}`, {
+    fallbackData: initialData,
+  });
+
+  const { trigger: update } = useProjectMutation(
+    id && `/dashboards/${id}`,
+    fetcher.patch,
+    {
+      onSuccess(data) {
+        mutate(data);
+        mutateViews();
+      },
+    },
+  );
+
+  const { trigger: remove } = useProjectMutation(
+    id && `/dashboards/${id}`,
+    fetcher.delete,
+    {
+      revalidate: false,
+      onSuccess() {
+        mutateViews();
+      },
+    },
+  );
+
+  return {
+    dashboard,
+    update,
+    remove,
+    mutate,
+    loading: isLoading,
+  };
+}
