6bd0f8c13df1e3f99383e9b0d576da2d177f60ad
7HR4IZ3 <gamerxville@gmail.com>
2024-11-19 21:10:19 -0800

    tests: export tests for log type csv and thread with export formats csv, jsonl and ojsonl

--- e2e/api.spec.ts
+++ e2e/api.spec.ts
@@ -1,75 +1,80 @@
-import { test, expect } from "@playwright/test"
+import { test, expect } from "@playwright/test";
 
-let privateKey = null
-let publicKey = null
+let privateKey: string | null;
+let publicKey: string | null = null;
 
-// run tests one after another
-test.describe.configure({ mode: "serial" })
-
-const apiUrl = process.env.API_URL || "http://localhost:3333"
+const apiUrl = process.env.API_URL || "http://localhost:3333";
 
 test("regenerate api keys", async ({ page }) => {
-  await page.goto("/settings")
+  await page.goto("/settings");
 
-  await page.waitForLoadState("networkidle")
+  await page.waitForLoadState("networkidle");
 
-  publicKey = await page.getByTestId("public-key").textContent()
+  publicKey = await page.getByTestId("public-key").textContent();
 
-  const firstPrivateKey = await page.getByTestId("private-key").textContent()
+  const firstPrivateKey = await page.getByTestId("private-key").textContent();
 
-  expect(publicKey).toHaveLength(36) // uuid length
-  expect(firstPrivateKey).toHaveLength(36) // uuid length
+  expect(publicKey).toHaveLength(36); // uuid length
+  expect(firstPrivateKey).toHaveLength(36); // uuid length
 
-  await page.waitForTimeout(300) // helps with flakiness in local
+  await page.waitForTimeout(300); // helps with flakiness in local
 
-  await page.getByTestId("regenerate-private-key-button").click()
+  await page.getByTestId("regenerate-private-key-button").click();
 
-  await page.getByTestId("confirm-button").click()
+  await page.getByTestId("confirm-button").click();
 
-  await page.waitForResponse((resp) => resp.url().includes("/regenerate-key"))
+  await page.waitForResponse((resp) => resp.url().includes("/regenerate-key"));
 
-  await page.waitForTimeout(1000)
+  await page.waitForTimeout(1500);
 
-  const secondPrivateKey = await page.getByTestId("private-key").textContent()
+  const secondPrivateKey = await page.getByTestId("private-key").textContent();
 
-  expect(firstPrivateKey).not.toEqual(secondPrivateKey)
+  expect(firstPrivateKey).not.toEqual(secondPrivateKey);
 
-  privateKey = secondPrivateKey
-})
+  console.log(publicKey, await page.getByTestId("public-key").textContent());
+
+  privateKey = secondPrivateKey;
+});
 
 test("private api /logs", async ({ page }) => {
   // Test API query
+
+  if (!privateKey) return;
 
   const res = await fetch(apiUrl + "/v1/runs", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      "X-API-Key": privateKey!,
+      "X-API-Key": privateKey,
     },
-  })
+  });
 
-  const json = await res.json()
-  expect(json.data).toBeInstanceOf(Array)
-})
+  const json = await res.json();
+  console.log(json);
+  expect(json.data).toBeInstanceOf(Array);
+});
 
 test("create dataset", async ({ page }) => {
   // Test API query
+
+  if (!privateKey) return;
 
   const res = await fetch(apiUrl + "/v1/datasets", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
-      "X-API-Key": privateKey!,
+      "X-API-Key": privateKey,
     },
     body: JSON.stringify({
       slug: "test-dataset",
       type: "chat",
     }),
-  })
+  });
 
-  const json = await res.json()
-  expect(json.slug).toEqual("test-dataset")
-})
+  const json = await res.json();
+  console.log(json);
+  expect(json.slug).toEqual("test-dataset");
+});
 
 test("get dataset publicly via slug", async ({ page }) => {
   // Test API query
@@ -79,10 +84,11 @@ test("get dataset publicly via slug", async ({ page }) => {
     headers: {
       // Use the legacy way to pass the API key (used in old SDKs)
       "Content-Type": "application/json",
-      Authorization: `Bearer ${publicKey!}`,
+      Authorization: `Bearer ${publicKey}`,
     },
-  })
+  });
 
-  const json = await res.json()
-  expect(json.items).toBeInstanceOf(Array)
-})
+  const json = await res.json();
+  console.log(json);
+  expect(json.items).toBeInstanceOf(Array);
+});
--- e2e/logs.spec.ts
+++ e2e/logs.spec.ts
@@ -7,6 +7,8 @@ import {
 import { setOrgPro } from "./utils/db";
 import fs from "fs";
 
+import { csv2json } from "json-2-csv";
+
 test.describe.configure({ mode: "serial" });
 
 let publicLogUrl: string;
@@ -29,7 +31,8 @@ test("make a log public", async ({ page, context }) => {
   });
 });
 
-test("test export csv", async ({ page }) => {
+// CSV EXPORTS
+test("test export csv - llm", async ({ page }) => {
   page.on("console", (msg) => {
     console.log(msg);
   });
@@ -47,7 +50,9 @@ test("test export csv", async ({ page }) => {
   const path = await file.path();
   const content = fs.readFileSync(path, "utf-8");
 
-  const expectedHeaders = [
+  const json = csv2json(content);
+
+  expect(Object.keys(json[0])).toEqual([
     "id",
     "projectId",
     "isPublic",
@@ -73,12 +78,263 @@ test("test export csv", async ({ page }) => {
     "user",
     "traceId",
     "scores",
-  ];
-  const actualHeaderLine = content.split("\n")[0].trim();
-  expect(actualHeaderLine).toBe(`"${expectedHeaders.join('","')}"`);
+  ]);
 
   // TODO: check that the content of each column for each row is correct (use json-2-csv to convert the csv to json
   // TODO: do the same thing for threads and traces (do not forget to check that children has the right)
+});
+
+// test("test export csv - trace", async ({ page }) => {
+//   page.on("console", (msg) => {
+//     console.log(msg);
+//   });
+//   await setOrgPro();
+
+//   await page.goto("/logs?type=trace");
+//   await page.waitForLoadState("networkidle");
+
+//   const downloadPromise = page.waitForEvent("download");
+
+//   await page.getByTestId("export-menu").click();
+//   await page.getByTestId("export-csv-button").click();
+
+//   const file = await downloadPromise;
+//   const path = await file.path();
+//   const content = fs.readFileSync(path, "utf-8");
+
+//   const logs: any[] = csv2json(content);
+
+//   console.log(logs);
+
+//   const expectedHeaders = TRACE_HEADERS;
+//   expect(Object.keys(logs[0])).toEqual(expectedHeaders);
+
+//   const logWithChildren = logs.find(
+//     (item: any) => item?.children?.length,
+//   )?.children;
+
+//   if (!logWithChildren) return;
+
+//   expect(Object.keys(logWithChildren[0])).toEqual(expectedHeaders);
+// });
+
+test("test export csv - thread", async ({ page }) => {
+  page.on("console", (msg) => {
+    console.log(msg);
+  });
+  await setOrgPro();
+
+  await page.goto("/logs?type=thread");
+  await page.waitForLoadState("networkidle");
+
+  const downloadPromise = page.waitForEvent("download");
+
+  await page.getByTestId("export-menu").click();
+  await page.getByTestId("export-csv-button").click();
+
+  const file = await downloadPromise;
+  const path = await file.path();
+  const content = fs.readFileSync(path, "utf-8");
+
+  const json = csv2json(content);
+
+  expect(Object.keys(json[0])).toEqual([
+    "id",
+    "projectId",
+    "isPublic",
+    "feedback",
+    "parentFeedback",
+    "type",
+    "name",
+    "createdAt",
+    "endedAt",
+    "duration",
+    "templateVersionId",
+    "templateSlug",
+    "cost",
+    "tokens",
+    "tags",
+    "input",
+    "output",
+    "error",
+    "status",
+    "siblingRunId",
+    "params",
+    "metadata",
+    "user",
+    "traceId",
+    "scores",
+  ]);
+});
+
+// RAW JSONL
+test("test export jsonl - llm", async ({ page }) => {
+  page.on("console", (msg) => {
+    console.log(msg);
+  });
+  await setOrgPro();
+
+  await page.goto("/logs?type=llm");
+  await page.waitForLoadState("networkidle");
+
+  const downloadPromise = page.waitForEvent("download");
+
+  await page.getByTestId("export-menu").click();
+  await page.getByTestId("export-raw-jsonl-button").click();
+
+  const file = await downloadPromise;
+  const path = await file.path();
+  const content = fs.readFileSync(path, "utf-8");
+
+  const json = JSON.parse(content.split("\n")[0]);
+
+  expect(Object.keys(json)).toEqual([
+    "id",
+    "createdAt",
+    "endedAt",
+    "duration",
+    "tags",
+    "projectId",
+    "status",
+    "name",
+    "error",
+    "input",
+    "output",
+    "params",
+    "type",
+    "parentRunId",
+    "promptTokens",
+    "completionTokens",
+    "cost",
+    "externalUserId",
+    "feedback",
+    "isPublic",
+    "templateVersionId",
+    "runtime",
+    "metadata",
+    "totalTokens",
+    "userId",
+    "userExternalId",
+    "userCreatedAt",
+    "userLastSeen",
+    "userProps",
+    "templateSlug",
+    "parentFeedback",
+    "evaluationResults",
+  ]);
+});
+
+// test("test export jsonl - trace", async ({ page }) => {
+//   page.on("console", (msg) => {
+//     console.log(msg);
+//   });
+//   await setOrgPro();
+
+//   await page.goto("/logs?type=trace");
+//   await page.waitForLoadState("networkidle");
+
+//   const downloadPromise = page.waitForEvent("download");
+
+//   await page.getByTestId("export-menu").click();
+//   await page.getByTestId("export-csv-button").click();
+
+//   const file = await downloadPromise;
+//   const path = await file.path();
+//   const content = fs.readFileSync(path, "utf-8");
+
+//   const json = JSON.parse(content.split("\n")[0]);
+
+//   const expectedHeaders = TRACE_HEADERS;
+//   expect(Object.keys(json)).toEqual(expectedHeaders);
+// });
+
+test("test export jsonl - thread", async ({ page }) => {
+  page.on("console", (msg) => {
+    console.log(msg);
+  });
+  await setOrgPro();
+
+  await page.goto("/logs?type=thread");
+  await page.waitForLoadState("networkidle");
+
+  const downloadPromise = page.waitForEvent("download");
+
+  await page.getByTestId("export-menu").click();
+  await page.getByTestId("export-raw-jsonl-button").click();
+
+  const file = await downloadPromise;
+  const path = await file.path();
+  const content = fs.readFileSync(path, "utf-8");
+
+  const json = JSON.parse(content.trim().split("\n")[0]);
+
+  expect(Object.keys(json)).toEqual([
+    "id",
+    "createdAt",
+    "endedAt",
+    "duration",
+    "tags",
+    "projectId",
+    "status",
+    "name",
+    "error",
+    "input",
+    "output",
+    "params",
+    "type",
+    "parentRunId",
+    "promptTokens",
+    "completionTokens",
+    "cost",
+    "externalUserId",
+    "feedback",
+    "isPublic",
+    "templateVersionId",
+    "runtime",
+    "metadata",
+    "totalTokens",
+    "userId",
+    "userExternalId",
+    "userCreatedAt",
+    "userLastSeen",
+    "userProps",
+    "templateSlug",
+    "parentFeedback",
+    "evaluationResults",
+  ]);
+});
+
+// OPENAI JSONL
+test("test export openai jsonl", async ({ page }) => {
+  page.on("console", (msg) => {
+    console.log(msg);
+  });
+  await setOrgPro();
+
+  await page.goto("/logs?type=llm");
+  await page.waitForLoadState("networkidle");
+
+  const downloadPromise = page.waitForEvent("download");
+
+  await page.getByTestId("export-menu").click();
+  await page.getByTestId("export-openai-jsonl-button").click();
+
+  const file = await downloadPromise;
+  const path = await file.path();
+  const content = fs.readFileSync(path, "utf-8");
+
+  const json: any = JSON.parse(content.trim().split("\n")[0]);
+
+  expect(json).toEqual(
+    expect.objectContaining({
+      messages: expect.arrayContaining([
+        expect.objectContaining({
+          role: expect.any(String),
+          content: expect.any(String),
+        }),
+      ]),
+    }),
+  );
 });
 
 // TODO: test export-raw-jsonl-button and export-openai-jsonl-button
