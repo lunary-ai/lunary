841b2e6304058fea0a373da46e8b0112f626c3dd
hughcrt <chocart.hugues@icloud.com>
2025-01-10 17:42:13 +0100

    ok

--- packages/db/0056.sql
+++ packages/db/0056.sql
@@ -1,7 +1,7 @@
 drop index if exists run_input_idx;
 drop index if exists run_output_idx;
 drop index if exists run_error_idx;
-drop table log;
+drop table if exists log;
 create extension if not exists btree_gin;
 create index run_project_id_input_idx on run using gin (project_id, ((input)::text) gin_trgm_ops);
 create index run_project_id_output_idx on run using gin (project_id, ((output)::text) gin_trgm_ops);
