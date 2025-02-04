efecefe75062c29a4f737eacd6d90998e2fa2a30
hughcrt <chocart.hugues@icloud.com>
2025-02-04 18:34:14 +0000

    fix

--- packages/frontend/utils/datatable.tsx
+++ packages/frontend/utils/datatable.tsx
@@ -42,6 +42,16 @@ export function durationColumn(unit = "s"): ColumnDef<any> {
     enableSorting: true,
     cell: (props) => {
       const value = props?.getValue() || 0;
+      const duration =
+        new Date(props.row.original.createdAt) -
+        new Date(props.row.original.endedAt);
+      const metadata = props.row.original.metadata?.cache;
+
+      const isCached = metadata?.cached || duration < 0.01 * 1000;
+
+      if (isCached) {
+        return "Cached";
+      }
 
       if (value === 0) {
         return "0.00s";
@@ -196,17 +206,6 @@ export function costColumn() {
     enableSorting: true,
     cell: (props) => {
       const cost = props.getValue();
-      const duration =
-        new Date(props.row.original.createdAt) -
-        new Date(props.row.original.endedAt);
-      const metadata = props.row.original.metadata?.cache;
-
-      const isCached = metadata?.cached || duration < 0.01 * 1000;
-
-      if (isCached) {
-        return "Cached";
-      }
-
       return <ProtectedText>{formatCost(cost)}</ProtectedText>;
     },
   });
