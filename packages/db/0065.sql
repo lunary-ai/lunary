3f24578c6fcf32b53daa1ad69593964e08cf10ab
Hugues Chocart <chocart.hugues@icloud.com>
2025-02-14 21:00:14 +0000

    feat: improve feedback filters perfs (#770)

---
+++ packages/db/0065.sql
@@ -0,0 +1,9 @@
+insert into _db_migration_async (name, operation, statement) values
+('run', 'alter-table',  
+'update run
+set
+    feedback = (feedback - ''thumbs'') || jsonb_build_object(''thumb'', feedback ->> ''thumbs'')
+where
+    feedback ->> ''thumbs'' is not null;'),
+('run', 'create', 
+'create index concurrently run_project_id_type_feedback_idx on run (project_id, type, (feedback->>''thumb''))');
