bcd15b0e878fe727343d778c0c21048f4948e746
Hugues Chocart <chocart.hugues@icloud.com>
2025-02-14 22:05:54 +0000

    ok

--- packages/db/0065.sql
+++ packages/db/0065.sql
@@ -5,5 +5,5 @@ set
     feedback = (feedback - ''thumbs'') || jsonb_build_object(''thumb'', feedback ->> ''thumbs'')
 where
     feedback ->> ''thumbs'' is not null;'),
-('run', 'create', 
-'create index concurrently run_project_id_type_feedback_idx on run (project_id, type, (feedback->>''thumb''))');
+('run_project_id_type_feedback_idx', 'drop', 'drop index concurrently if exists run_project_id_type_feedback_idx'),
+('run_project_id_type_feedback_idx', 'create', 'create index concurrently if not exists run_project_id_type_feedback_idx on run (project_id, type, (feedback->>''thumb''))');
