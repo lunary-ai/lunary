aea059c2eb40fe0bd32b1b534f16f4d971287254
Hugues Chocart <chocart.hugues@icloud.com>
2025-02-23 22:51:20 +0000

    testw

--- packages/e2e/playwright.config.ts
+++ packages/e2e/playwright.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig, devices } from "@playwright/test";
 
 /**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-import dotenv from "dotenv";
-
-// Read from default ".env" file.
-dotenv.config({ path: "../backend/.env" });
-
-/**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
