ee1ce467b016e9b4ef343ff69127dad85b37d0d6
Hugues Chocart <chocart.hugues@icloud.com>
2025-03-06 00:50:37 +0100

    ok

--- packages/backend/src/api/v1/openapi.ts
+++ packages/backend/src/api/v1/openapi.ts
@@ -1,15 +1,28 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import Router from "koa-router";
 
-const options = {
+const options: swaggerJsdoc.Options = {
   definition: {
     openapi: "3.0.0",
     info: {
       title: "Lunary API",
       version: "1.0.0",
     },
+    servers: [
+      {
+        url: "https://api.lunary.ai",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        BearerAuth: {
+          type: "http",
+          scheme: "bearer",
+        },
+      },
+    },
   },
-  apis: ["./src/api/v1/**/*.ts"], // files containing annotations as above
+  apis: ["./src/api/v1/**/*.ts"],
 };
 
 const openapiSpecification = swaggerJsdoc(options);
