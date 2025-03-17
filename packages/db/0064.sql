8933277813efc3ac34e249f98fc454b41abc9a9f
Hugues Chocart <chocart.hugues@icloud.com>
2025-03-17 02:50:05 +0100

    feat: fix migration (#830)

--- packages/db/0064.sql
+++ packages/db/0064.sql
@@ -1,2 +1,2 @@
 insert into _db_migration_async (name, operation, statement) values
-('run', 'alter-table', 'alter table run alter column run set not null');
+('run', 'alter-table', 'alter table run alter column type set not null');
