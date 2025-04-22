f038844b2ca2ccbdc996485e07a4c078fa63d91d
Hugues Chocart <chocart.hugues@icloud.com>
2025-04-22 16:43:54 +0200

    ok

--- packages/db/0084.sql
+++ packages/db/0084.sql
@@ -1,1 +1,12 @@
-alter table org_invitation add column email_verified boolean default null;
+create table org_invitation (
+  id uuid primary key default gen_random_uuid(),
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now(),
+  email text not null,
+  org_id uuid not null,
+  role user_role not null,
+  token text not null,
+  email_verified boolean,
+  constraint org_invitation_account_id_fkey foreign key (org_id) references org(id) on delete cascade
+  primary key (id)
+);
