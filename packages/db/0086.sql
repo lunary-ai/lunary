3ecdfa24e18f2a3767ccedb6033a4a4da8d04e80
Hugues Chocart <chocart.hugues@icloud.com>
2025-04-24 00:19:41 +0200

    feat: filter performance improvements (#878)

---
+++ packages/db/0086.sql
@@ -0,0 +1,5 @@
+drop index if exists run_project_id_cost_idx;
+
+insert into _db_migration_async (name, operation, statement) values
+('run_project_id_template_version_id_idx', 'create', 'create index concurrently if not exists run_project_id_template_version_id_idx on run (project_id, template_version_id)'),
+('run_project_name_created_at_desc_idx', 'create', 'create index concurrently if not exists run_project_name_created_at_desc_idx on run (project_id, name, created_at desc nulls last)');
