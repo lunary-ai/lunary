61f31041c5c530ae2a83a4c9ae39130705db17b8
Hugues Chocart <chocart.hugues@icloud.com>
2025-04-28 08:43:28 +0200

    feat: replace crisp by intercom (#894)

--- packages/frontend/utils/config.ts
+++ packages/frontend/utils/config.ts
@@ -1,6 +1,7 @@
 const config = {
   IS_SELF_HOSTED:
     process.env.NEXT_PUBLIC_IS_SELF_HOSTED === "true" ? true : false,
+  IS_CLOUD: process.env.NEXT_PUBLIC_IS_SELF_HOSTED !== "true",
   RECAPTCHA_SITE_KEY: "6LeDTNsqAAAAANXZDQATUpJ8s1vttWkZabP6g3O6",
 };
 
