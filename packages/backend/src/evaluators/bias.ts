0106a697db3a560499bcda344dc71b143ccbf39c
Hugues Chocart <chocart.hugues@icloud.com>
2025-05-01 21:14:16 +0200

    feat: improve evaluators (#922)
    Signed-off-by: Hugues Chocart <chocart.hugues@icloud.com>

--- packages/backend/src/evaluators/bias.ts
+++ packages/backend/src/evaluators/bias.ts
@@ -1,67 +1,36 @@
+import { callML } from "@/src/utils/ml";
 import { Run } from "shared";
-import { callML } from "../utils/ml";
+import { z } from "zod";
+import { parseMessages } from "./utils";
+import { cat } from "@xenova/transformers";
 
-interface BiasParams {
-  threshold?: number;
+const biasSchema = z.array(
+  z.union([z.null(), z.object({ reason: z.string() })]),
+);
+type Toxicity = z.infer<typeof toxicitySchema>;
+
+export interface ToxicityEvaluation {
+  input: Toxicity;
+  output: Toxicity;
 }
 
-function parseMessages(messages: unknown): string[] {
-  if (!messages) return [""];
-
-  if (typeof messages === "string") {
-    if (!messages.length || messages === "__NOT_INGESTED__") return [""];
-    return [messages];
-  }
-
-  if (Array.isArray(messages)) {
-    const contentArray: string[] = [];
-    for (const message of messages) {
-      if (message?.type === "system") continue;
-      const content = message.content ?? message.text;
-      if (typeof content === "string" && content.length) {
-        contentArray.push(content);
-      } else {
-        contentArray.push(JSON.stringify(message));
-      }
-    }
-    return contentArray.length ? contentArray : [""];
-  }
-
-  if (typeof messages === "object") {
-    return [JSON.stringify(messages)];
-  }
-
-  return [""];
+async function getToxicity(texts: string[]): Promise<Toxicity> {
+  const raw = await callML("toxicity", { texts });
+  return toxicitySchema.parse(raw);
 }
 
-export async function evaluate(run: Run, params: BiasParams) {
-  const { threshold = 0.5 } = params;
-
+export async function evaluate(run: Run): Promise<ToxicityEvaluation | null> {
   const inputTexts = parseMessages(run.input);
   const outputTexts = parseMessages(run.output);
-  const errorTexts = parseMessages(run.error);
 
-  const [inputBias, outputBias, errorBias] = await Promise.all([
-    detectBias(inputTexts, threshold),
-    detectBias(outputTexts, threshold),
-    detectBias(errorTexts, threshold),
-  ]);
-
-  return {
-    input: inputBias,
-    output: outputBias,
-    error: errorBias,
-  };
-}
-
-async function detectBias(texts: string[], threshold: number): Promise<any> {
   try {
-    return callML("bias", {
-      outputs: texts,
-      threshold,
-    });
+    const [input, output] = await Promise.all([
+      inputTexts ? getToxicity(inputTexts) : Promise.resolve([null]),
+      outputTexts ? getToxicity(outputTexts) : Promise.resolve([null]),
+    ]);
+    return { input, output };
   } catch (error) {
     console.error(error);
-    console.error(texts);
+    return null;
   }
 }
