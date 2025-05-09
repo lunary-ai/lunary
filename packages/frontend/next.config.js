5df6947dc25d01a6f219b043849f6c6b1bcbf382
Hugues Chocart <chocart.hugues@icloud.com>
2025-05-09 17:30:14 +0200

    Revert "feat: add sentry on frontend (#952)"
    This reverts commit 10c2e7c64e7ee1aed458b86e8b797045ae9afa92.

--- packages/frontend/next.config.js
+++ packages/frontend/next.config.js
@@ -1,8 +1,5 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import config from "./utils/config";
-
 /** @type {import('next').NextConfig} */
-const baseConfig = {
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
@@ -75,40 +72,4 @@ const baseConfig = {
   transpilePackages: ["shared"],
 };
 
-const sentryConfig = withSentryConfig(baseConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: "lunary-5a",
-  project: "production",
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: "/monitoring",
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
-  automaticVercelMonitors: true,
-
-  reactComponentAnnotation: {
-    enabled: true,
-  },
-});
-
-export default config.IS_CLOUD ? sentryConfig : baseConfig;
+module.exports = nextConfig;
