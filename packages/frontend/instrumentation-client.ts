a152f8d194a889a6fab55fa959f509ee272daeae
Hugues Chocart <chocart.hugues@icloud.com>
2025-05-09 16:38:27 +0200

    feat: add sentry on frontend (#952)

---
+++ packages/frontend/instrumentation-client.ts
@@ -0,0 +1,31 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+import config from "./utils/config";
+
+Sentry.init({
+  dsn: config.IS_CLOUD
+    ? "https://c714dfeb448caef6e12e281ad1e9250e@o4509293498728448.ingest.de.sentry.io/4509293501808720"
+    : null,
+
+  // Add optional integrations for additional features
+  integrations: [Sentry.replayIntegration()],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Define how likely Replay events are sampled.
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // Define how likely Replay events are sampled when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
---
+++ packages/frontend/instrumentation.ts
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/nextjs";
+import config from "./utils/config";
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    await import("./sentry.server.config");
+  }
+
+  if (process.env.NEXT_RUNTIME === "edge") {
+    await import("./sentry.edge.config");
+  }
+}
+
+export const onRequestError = Sentry.captureRequestError;
