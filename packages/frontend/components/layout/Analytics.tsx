64fb3fcaeac9cc16574222faca8b9e10eb0a1dcb
Hugues Chocart <chocart.hugues@icloud.com>
2025-05-30 16:18:33 +0800

    ok

--- packages/frontend/components/layout/Analytics.tsx
+++ packages/frontend/components/layout/Analytics.tsx
@@ -1,6 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, Component } from "react";
-import Script from "next/script";
+import { useEffect } from "react";
 
 import { PostHogProvider } from "posthog-js/react";
 
@@ -20,24 +19,9 @@ export default function AnalyticsWrapper({ children }) {
     };
   }, []);
 
-  return (
-    <>
-      {process.env.NEXT_PUBLIC_CUSTOM_SCRIPT && (
-        <Script
-          id="custom-script"
-          dangerouslySetInnerHTML={{
-            __html: process.env.NEXT_PUBLIC_CUSTOM_SCRIPT,
-          }}
-          onLoad={() => console.info("Custom script loaded.")}
-          onError={() => console.info("Custom script failed to load.")}
-        />
-      )}
-
-      {process.env.NEXT_PUBLIC_POSTHOG_KEY ? (
-        <PostHogProvider client={posthog}>{children}</PostHogProvider>
-      ) : (
-        children
-      )}
-    </>
+  return process.env.NEXT_PUBLIC_POSTHOG_KEY ? (
+    <PostHogProvider client={posthog}>{children}</PostHogProvider>
+  ) : (
+    children
   );
 }
