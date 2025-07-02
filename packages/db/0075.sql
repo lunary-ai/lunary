088e336f82d55ab7bf7277a76c531d3e0fb51978
Hugues Chocart <chocart.hugues@icloud.com>
2025-07-02 02:14:24 +0200

    fix

--- packages/db/0075.sql
+++ packages/db/0075.sql
@@ -1,2 +1,2 @@
 insert into _db_migration_async (name, operation, statement) values
-('run_metadata_idx', 'create', 'create index concurrently run_metadata_idx if not exists on run using gin (metadata jsonb_path_ops)');
+('run_metadata_idx', 'create', 'create index concurrently run_metadata_idx on run using gin (metadata jsonb_path_ops)');
