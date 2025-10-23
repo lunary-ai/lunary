ac1b3be2caa4f0185815d5acef76753d1d8de588
hughcrt <chocart.hugues@icloud.com>
2025-10-23 12:21:58 +0200

    fix build

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -3,11 +3,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
