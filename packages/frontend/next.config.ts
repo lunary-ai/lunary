59eff1970422eed3186a7aa1eca32d9dbe637037
hughcrt <chocart.hugues@icloud.com>
2025-10-23 13:22:16 +0200

    chores

--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -4,10 +4,6 @@ import { withSentryConfig } from "@sentry/nextjs";
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
   },
   images: {
@@ -77,30 +73,14 @@ const nextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
   org: "lunary-0i",
   project: "frontend",
 
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-  _experimental: {
-    useRunAfterProductionCompileHook: true,
-  },
+  useRunAfterProductionCompileHook: true,
 
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
 
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
